e3d860bd82a8227bcc314dc60d10d596dc7ce143
leiguangwu <guangwu.lei@zuolin.com>
2019-08-21 14:54:52 +0800

    新增图片本地保存功能

--- app/src/main/java/com/fitem/ktgames/games/ui/grils/activity/GirlsActivity.kt
+++ app/src/main/java/com/fitem/ktgames/games/ui/grils/activity/GirlsActivity.kt
@@ -1,12 +1,22 @@
 package com.fitem.ktgames.games.ui.grils.activity
 
-import android.graphics.Color
+import android.content.Intent
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.util.TypedValue
+import android.view.Menu
 import android.view.MenuItem
 import android.view.ViewGroup
 import android.view.animation.DecelerateInterpolator
+import com.blankj.utilcode.util.EncryptUtils
+import com.blankj.utilcode.util.FileUtils
+import com.blankj.utilcode.util.ToastUtils
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.fitem.ktgames.R
 import com.fitem.ktgames.common.base.BaseActivity
 import com.fitem.ktgames.common.utils.SystemUiVisibilityUtil
@@ -14,6 +24,9 @@ import com.fitem.ktgames.common.widget.PullBackLayout
 import com.fitem.ktgames.games.ui.main.Constants
 import com.hazz.kotlinmvp.glide.GlideApp
 import kotlinx.android.synthetic.main.activity_girls.*
+import java.io.File
+import java.io.FileNotFoundException
+
 
 /**
  * Created by LeiGuangwu on 2019-08-14.
@@ -22,8 +35,9 @@ class GirlsActivity : BaseActivity(), PullBackLayout.Callback {
 
     private var mIsToolBarHidden = false
     private var mIsStatusBarHidden = false
-    private var mBackground: ColorDrawable = ColorDrawable(Color.BLACK)
+    private var mBackground: ColorDrawable? = null
     private var url: String? = null
+    private var fileTypeStr: String? = null
 
     override fun onPullStart() {
         toolBarFadeOut()
@@ -33,7 +47,7 @@ class GirlsActivity : BaseActivity(), PullBackLayout.Callback {
 
     override fun onPull(progress: Float) {
         val newProgress = Math.min(1f, progress * 3f)
-        mBackground.alpha = (0xff/*255*/ * (1f - newProgress)).toInt()
+        mBackground?.alpha = (0xff/*255*/ * (1f - newProgress)).toInt()
     }
 
     override fun onPullCancel() {
@@ -44,16 +58,20 @@ class GirlsActivity : BaseActivity(), PullBackLayout.Callback {
         supportFinishAfterTransition()
     }
 
-    override fun getLayoutId(): Int = R.layout.activity_girls;
+    override fun getLayoutId(): Int = R.layout.activity_girls
 
     override fun parseArgument() {
         url = intent.getStringExtra(Constants.GIRLS_URL)
+        mBackground = ColorDrawable(getColorPrimary())
+        fileTypeStr = url?.let {
+            val list = it.split(".")
+            "." + list[list.size - 1]
+        }
     }
 
     override fun initView() {
 //        StatusBarCompat.setStatusBarColor(this, ContextCompat.getColor(this, R.color.black_gray))
         mToolbar.setTitle(R.string.girls)
-        mToolbar.setBackgroundColor(Color.BLACK)
         findViewById<ViewGroup>(android.R.id.content).getChildAt(0).setBackgroundDrawable(mBackground)
         setSupportActionBar(mToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -100,11 +118,92 @@ class GirlsActivity : BaseActivity(), PullBackLayout.Callback {
         hideOrShowToolbar()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_girls, menu)
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId == android.R.id.home){
-            onBackPressed()
+        when (item?.itemId) {
+            android.R.id.home
+            -> onBackPressed()
+            R.id.save
+            -> url?.let { download(it) }
         }
         return super.onOptionsItemSelected(item)
     }
 
+    private fun getColorPrimary(): Int {
+        val typedValue = TypedValue()
+        theme.resolveAttribute(R.attr.colorPrimary, typedValue, true)
+        return typedValue.data
+    }
+
+    // 保存图片到手机
+    fun download(url: String) {
+        val pathDir = Constants.DOWNLOAD_DIR
+        val pathName = pathDir + File.separator +
+                EncryptUtils.encryptMD5ToString(url) + fileTypeStr
+        if(FileUtils.isFileExists(pathName)){
+            return ToastUtils.showShort("该图片已存在！")
+        }
+        var mRequestManager = GlideApp.with(this)
+        var mRequestBuilder = mRequestManager
+            .downloadOnly()
+            .load(url)
+            .listener(object : RequestListener<File> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<File>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: File?,
+                    model: Any?,
+                    target: Target<File>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    try {
+                        FileUtils.createOrExistsDir(pathDir).let {
+                            FileUtils.createOrExistsFile(pathName).let {
+                                var file = FileUtils.getFileByPath(pathName)
+                                resource?.apply {
+                                    val success = FileUtils.copyFile(resource, file)
+                                    val resultStr = if (success) "下载成功，目录：$pathName" else "下载失败"
+                                    ToastUtils.showLong(resultStr)
+                                    if(success){
+                                        updateMediaStore(file)
+                                    }
+                                }
+                            }
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+
+                    return false
+                }
+
+            })
+
+        mRequestBuilder.preload()
+    }
+
+    private fun updateMediaStore(file: File) {
+        if (file.exists()) {
+            try {
+                val intent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+                val uri = Uri.fromFile(file)
+                intent.data = uri
+                sendBroadcast(intent)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
--- app/src/main/java/com/fitem/ktgames/games/ui/main/Constants.kt
+++ app/src/main/java/com/fitem/ktgames/games/ui/main/Constants.kt
@@ -1,5 +1,9 @@
 package com.fitem.ktgames.games.ui.main
 
+import android.os.Environment
+import com.blankj.utilcode.util.AppUtils
+import java.io.File
+
 /**
  * Created by LeiGuangwu on 2019-07-12.
  */
@@ -7,5 +11,7 @@ object Constants {
 
     const val CURRENT_TAB_INDEX = "current_tab_index"
     const val GIRLS_URL = "girls_url"
-
+    @JvmField
+    val DOWNLOAD_DIR = Environment.getExternalStorageDirectory().absolutePath +
+            File.separator + AppUtils.getAppName() + File.separator + "Download"
 }
